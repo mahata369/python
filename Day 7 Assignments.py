aaea1da200051985f34f4d6b1a570586b572f256
mahata369 <139675523+mahata369@users.noreply.github.com>
2026-01-31 19:54:28 +0545

    Add files via upload

---
+++ Day 7 Assignments.py
@@ -0,0 +1,26 @@
+#Day 7 Assignments
+# create a list of even numbers from 1-20
+# even_num=[i for i in range(1,21) if i %2 == 0]
+# print(even_num)
+
+# create a list of odd numbers from 1-20
+# Odd_num=[i for i in range(1,21) if i %2 != 0]
+# print(Odd_num)
+
+# create a list of square of numbers from 1-20
+# square=[i*i for i in range(1,21) ]
+# print(square)
+
+# create a list of lenght of each words in ["apple","orange","kiwi"]
+# fruits=["apple","orange","kiwi"]
+# _fruits=[len(word) for word in fruits]
+# print(_fruits)
+
+# create a list of squares of only even numbers from 1-20
+# square=[i*i for i in range(1,21) if i %2 == 0]
+# print(square)
+
+# create a list of numbers divisible by 3 from 1-100
+# div_by_3=[i for i in range(1,101) if i % 3 == 0]
+# print(div_by_3)
+
