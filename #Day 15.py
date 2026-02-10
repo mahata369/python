c012e8aedca209b99b3670a76f0049ca505cb0b5
mahata369 <anujmahata999@gmail.com>
2026-02-10 20:45:17 +0545

    c

--- #Day 15.py
+++ #Day 15.py
@@ -23,16 +23,46 @@ Types of error:
 # except PermissionError:
 #     print("Permission denied")
 
+# try:
+#     n = int(input("Enter a number ")) #Exception generation codes are written inside try
+#     print(100/n)
+
+# except ZeroDivisionError:
+#     print("Cannot divide by zero")
+
+# else:
+#     print("No error")
+
+# finally:
+#     print("this is finally block")
+
+'''
+ZeroDivisonByZero:
+TypeError:
+
+
+'''
+
+#TypeError
+# a= 10 # int
+# b="10" #string
+# try:
+#     print(a+b)
+# except TypeError:
+#     print("Type error occured")
+
+#ValueError
+# try:
+#     num = int("abc")
+#     print(num)
+# except ValueError:
+#     print("ValueError: Invalid value for typecast")
+
+# NameError
 try:
-    n = int(input("Enter a number "))#Exception generation codes
-    print(100/n)
-
-except ZeroDivisionError:
-    print("Cannot divide by zero")
-
-else:
-    print("No error")
-
-finally:
-    print("this is finally block")
+   print(x)
+    
+except NameError:
+    print("NameError: variable is not defined")
+     
 
