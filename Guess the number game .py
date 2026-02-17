ac81488a4c6b8879935191e9d58bb7104f5f8412
mahata369 <anujmahata999@gmail.com>
2026-02-17 18:12:10 +0545

    ok

--- Guess the number game .py
+++ Guess the number game .py
@@ -58,65 +58,65 @@
 #         break
 
 #Guess the number game v3
-import random
+# import random
 
-def choose_difficulty():
-    print("\nChoose Difficulty:")
-    print("1. Easy (1-50, 10 attempts)")
-    print("2. Medium (1-100, 7 attempts)")
-    print("3. Hard (1-200,5 attempts)")
+# def choose_difficulty():
+#     print("\nChoose Difficulty:")
+#     print("1. Easy (1-50, 10 attempts)")
+#     print("2. Medium (1-100, 7 attempts)")
+#     print("3. Hard (1-200,5 attempts)")
 
-    while True:
-        choice = input("Enter 1/2/3: ")
-        if choice == "1":
-            return 50,10,"Easy"
-        elif choice == "2":
-            return 100, 7, "Medium"
-        elif choice =="3":
-            return 200, 5, "Hard"
-        else:
-            print("Invalid choice. Please enter 1,2 or 3.")
+#     while True:
+#         choice = input("Enter 1/2/3: ")
+#         if choice == "1":
+#             return 50,10,"Easy"
+#         elif choice == "2":
+#             return 100, 7, "Medium"
+#         elif choice =="3":
+#             return 200, 5, "Hard"
+#         else:
+#             print("Invalid choice. Please enter 1,2 or 3.")
 
-def get_valid_guess(Prompt):
-    while True:
-        value = input(Prompt)
-        if value.isdigit():
-            return int(value)
-        else:
-            print("Please enter a valid number!")
+# def get_valid_guess(Prompt):
+#     while True:
+#         value = input(Prompt)
+#         if value.isdigit():
+#             return int(value)
+#         else:
+#             print("Please enter a valid number!")
 
-print("Welcome to Guess The Number Game v3!")
+# print("Welcome to Guess The Number Game v3!")
 
-while True:
-    max_range, max_attempts, difficulty = choose_difficulty()
-    secret_number = random.randint(1, max_range)
+# while True:
+#     max_range, max_attempts, difficulty = choose_difficulty()
+#     secret_number = random.randint(1, max_range)
 
-    attempts = 0
-    print(f"\n Difficulty: {difficulty}")
-    print(f"I have selected a number between 1 and {max_range}.")
-    print(f"You have {max_attempts} attempts. Good luck")
+#     attempts = 0
+#     print(f"\n Difficulty: {difficulty}")
+#     print(f"I have selected a number between 1 and {max_range}.")
+#     print(f"You have {max_attempts} attempts. Good luck")
 
-    won = False
+#     won = False
 
-    while attempts < max_attempts:
-        remaining = max_attempts - attempts
-        print(f"\n Attempts left: {remaining}")
+#     while attempts < max_attempts:
+#         remaining = max_attempts - attempts
+#         print(f"\n Attempts left: {remaining}")
 
-        guess =get_valid_guess("Enter your guess: ")
+#         guess =get_valid_guess("Enter your guess: ")
 
-        if guess > secret_number:
-            print(" Too high!")
-        elif guess < secret_number:
-            print(" Too low!")
-        else:
-            print(f"\n Congratulations! You guessed it in {attempts} attempts.")
-            won = True
-            break
-    if not won:
-        print("\n Game over!")
-        print(f"The correct number was: {secret_number}")
+#         if guess > secret_number:
+#             print(" Too high!")
+#         elif guess < secret_number:
+#             print(" Too low!")
+#         else:
+#             print(f"\n Congratulations! You guessed it in {attempts} attempts.")
+#             won = True
+#             break
+#     if not won:
+#         print("\n Game over!")
+#         print(f"The correct number was: {secret_number}")
 
-    play_again =input("\nDo you want to play again?")
-    if play_again != "y":
-        print("Bye! See you next time")
-        break
+#     play_again =input("\nDo you want to play again?")
+#     if play_again != "y":
+#         print("Bye! See you next time")
+#         break
